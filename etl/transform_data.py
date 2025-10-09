94e782b664d8695e7d5f590c137bc5bd2b049df1
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-10-08 22:08:07 -0300

    Initial commit

--- etl/transform_data.py
+++ etl/transform_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import time
 from serpapi import GoogleSearch
-from etl.utils import preenche_campos, normaliza_campos, atualizar_lat_long
+from etl.utils import preenche_campos, normaliza_campos
 
 
 IBGE_CSV = "data/utils/ibge_data.csv"
@@ -16,8 +16,6 @@ def transform_data_silver(csv_path):
     #df_silver = df_silver.tail(1)
     
     df_silver = preenche_campos(df_silver, IBGE_CSV)
-
-    df_silver = normaliza_campos(df_silver)
     
     return df_silver
 
@@ -27,6 +25,6 @@ def transform_data_gold(csv_path):
 
     df_gold = pd.read_csv(csv_path, sep=";", encoding="utf-8")
 
-    df_gold = atualizar_lat_long(df_gold)
+    df_gold = normaliza_campos(df_gold)
 
     return df_gold
